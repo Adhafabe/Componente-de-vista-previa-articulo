7368b5e0b39f9bfd396b15501149aae0c35efbe6
Adarely <adhafabe@gmail.com>
2020-07-22 22:22:53 -0500

    Diseño escritorio

---
+++ main.js
@@ -0,0 +1,10 @@
+$("#iconHover").click(function(){ 
+    $(".article__autor").addClass("active");
+ }   
+); 
+
+$("#iconHoverB").click(function(){ 
+    $(".article__autor").removeClass("active");
+ }   
+); 
+
